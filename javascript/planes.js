3a8a62eb19f83e80a0db012f62ba525304f1b629
TuNombreDeUsuario <dgarzoncontacto@gmail.com>
2023-11-24 03:09:18 -0500

    pantalla princ terminada

--- javascript/planes.js
+++ javascript/planes.js
@@ -1,3 +1,17 @@
+//para el menu en responsive
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const navList = document.getElementById('nav-list');
+  
+    menuIcon.addEventListener('click', function () {
+        navList.classList.toggle('show');
+    });
+  });
+  
+
+//para el cambio de pestañas 
+
 function showTab(tabId) {
     // Oculta todas las pestañas
     document.querySelectorAll('.tab-content').forEach(tab => {
@@ -5,11 +19,23 @@ function showTab(tabId) {
     });
 
     // Muestra la pestaña seleccionada
-    document.getElementById(tabId).classList.add('active');
+    const selectedTab = document.getElementById(tabId);
+    selectedTab.classList.add('active');
 
     // Ajusta la posición de las pestañas
     document.querySelectorAll('.tab').forEach(tab => {
         tab.classList.remove('active');
     });
-    document.getElementById(`tab${tabId.charAt(0).toUpperCase() + tabId.slice(2)}`).classList.add('active');
+
+    const tabElement = document.getElementById(`tab${tabId.charAt(0).toUpperCase() + tabId.slice(1)}`);
+    tabElement.classList.add('active');
+
+    // Intercambia las clases de las pestañas
+    if (tabElement.classList.contains('tab-premium')) {
+        document.getElementById('tabBasic').classList.add('active');
+        document.getElementById('tabPremium').classList.remove('active');
+    } else {
+        document.getElementById('tabPremium').classList.add('active');
+        document.getElementById('tabBasic').classList.remove('active');
+    }
 }
